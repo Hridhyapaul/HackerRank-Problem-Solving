acf19868f97681ffc3954ad48dd7cb6b69609d25
Hridhyapaul <hpdesign2021@gmail.com>
2023-10-15 23:36:02 -1200

    Second Solution of Birthday-Cake-Candles

--- Birthday-Cake-Candles/Solution.js
+++ Birthday-Cake-Candles/Solution.js
@@ -7,3 +7,17 @@ const numberOfMaxValue = candles.filter(num => num === maxValue)
 const count = numberOfMaxValue.length
 return count;
 }
+
+// ============ Solution 02 ============
+
+function birthdayCakeCandles(candles) {
+    // Write your code here
+    let maxValue = Math.max(...candles)
+    let count = 0;
+    for (let i = 0; i < candles.length; i++) {
+        if (candles[i] === maxValue) {
+            count++;
+        }
+    }
+    return count
+}
